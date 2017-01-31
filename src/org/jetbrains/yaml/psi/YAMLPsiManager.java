2860d320068fc2b18b05e3c0afa693cdc8832c33
VISTALL <vistall.valeriy@gmail.com>
2017-01-31 17:25:51 +0300

    compilation

--- src/org/jetbrains/yaml/psi/YAMLPsiManager.java
+++ src/org/jetbrains/yaml/psi/YAMLPsiManager.java
@@ -1,11 +1,11 @@
 package org.jetbrains.yaml.psi;
 
 import org.jetbrains.annotations.NotNull;
-import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiFileSystemItem;
+import com.intellij.psi.PsiManager;
+import com.intellij.psi.impl.PsiTreeChangeEventImpl;
 import com.intellij.psi.impl.PsiTreeChangePreprocessorBase;
 
 /**
@@ -13,49 +13,36 @@ import com.intellij.psi.impl.PsiTreeChangePreprocessorBase;
  */
 final class YAMLPsiManager extends PsiTreeChangePreprocessorBase
 {
-	public YAMLPsiManager(@NotNull Project project)
+	public YAMLPsiManager(@NotNull PsiManager psiManager)
 	{
-		super(project);
+		super(psiManager);
 	}
 
 	@Override
-	protected boolean isInsideCodeBlock(PsiElement element)
+	protected boolean acceptsEvent(@NotNull PsiTreeChangeEventImpl event)
 	{
-		if(element instanceof PsiFileSystemItem)
-		{
-			return false;
-		}
+		return event.getFile() instanceof YAMLFile;
+	}
 
-		if(element == null || element.getParent() == null)
-		{
-			return true;
-		}
-
+	@Override
+	protected boolean isOutOfCodeBlock(@NotNull PsiElement element)
+	{
 		while(true)
 		{
 			if(element instanceof YAMLFile)
 			{
-				return false;
+				return true;
 			}
 			if(element instanceof PsiFile || element instanceof PsiDirectory)
 			{
-				return true;
+				return false;
 			}
 			PsiElement parent = element.getParent();
-			if(!(parent instanceof YAMLFile ||
-					parent instanceof YAMLKeyValue ||
-					parent instanceof YAMLCompoundValue ||
-					parent instanceof YAMLDocument))
+			if(!(parent instanceof YAMLFile || parent instanceof YAMLKeyValue || parent instanceof YAMLCompoundValue || parent instanceof YAMLDocument))
 			{
-				return true;
+				return false;
 			}
 			element = parent;
 		}
-	}
-
-	@Override
-	protected boolean isMyFile(@NotNull PsiFile psiFile)
-	{
-		return psiFile instanceof YAMLFile;
 	}
 }
