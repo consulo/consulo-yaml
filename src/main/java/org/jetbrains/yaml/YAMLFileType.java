30130755042d942c19c3ad7c5bf0527530ded749
VISTALL <vistall.valeriy@gmail.com>
2021-12-03 13:07:00 +0300

    update icon

--- src/main/java/org/jetbrains/yaml/YAMLFileType.java
+++ src/main/java/org/jetbrains/yaml/YAMLFileType.java
@@ -1,12 +1,11 @@
 package org.jetbrains.yaml;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-
-import org.jetbrains.annotations.NonNls;
-import com.intellij.icons.AllIcons;
 import com.intellij.openapi.fileTypes.LanguageFileType;
 import consulo.ui.image.Image;
+import consulo.yaml.icon.YAMLIconGroup;
+import org.jetbrains.annotations.NonNls;
+
+import javax.annotation.Nonnull;
 
 public class YAMLFileType extends LanguageFileType {
   public static final YAMLFileType YML = new YAMLFileType();
@@ -33,9 +32,9 @@ public class YAMLFileType extends LanguageFileType {
     return DEFAULT_EXTENSION;
   }
 
-  @Nullable
+  @Nonnull
   public Image getIcon() {
-    return AllIcons.Nodes.DataTables;
+    return YAMLIconGroup.yaml();
   }
 }
 
